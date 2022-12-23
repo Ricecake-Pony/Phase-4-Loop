64121a53c54711027d889304ce044833cfa6ae73
Trey <trey.delcamp@gmail.com>
2022-12-23 09:42:25 -0600

    seeds working!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :event_settings
     has_many :events, through: :event_settings
+
+    has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,26 @@
 puts "Clearing DB"
 User.destroy_all
 Event.destroy_all
-Event_settings.destroy_all
+EventSetting.destroy_all
 puts "DB cleared!"
 
 puts "creating the users!"
-5.times {User.create(name:)}
+5.times {User.create(
+    name: Faker::FunnyName.name, 
+    username:Faker::Name.first_name, 
+    birthdate: Faker::Date.between(from: '1903-09-23', to: '2022-09-25'), password: "1")}
+    
+    puts "Users created yo!"
+    
+    puts "Events being planned"
+    strings = ["casual","business casual", "business"]
+    10.times {Event.create(title:"Go to" + Faker::Mountain.name, 
+    description: Faker::Quotes::Shakespeare.hamlet_quote,
+    attire: strings.sample)}
+    puts "Events planned my man!"
 
+    puts " Looking at Event Settings"
+    8.times {EventSetting.create(plus_one: Faker::Boolean.boolean, user: User.all.sample, event: Event.all.sample )}
+    puts "Event Settings Set, bro!"
+
+    puts "Seeding done!"
