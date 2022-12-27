b1f042338253166ded6f77ca743012574aa6f796
Trey <trey.delcamp@gmail.com>
2022-12-27 15:56:23 -0600

    Created Signup/Login on Frontend!

---
+++ client/src/SignUp.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+function SignUp({ setUser }) {
+    const [username, setUsername] = useState("");
+    const [name, setName] = useState("");
+    const [birthdate, setBirthdate] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+    function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/signup", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        name,
+        username,
+        birthdate,
+        password,
+        password_confirmation: passwordConfirmation,
+        }),
+    }).then((r) => {
+        if (r.ok) {
+        r.json().then((user) => setUser(user));
+        }
+    });
+    }
+
+    return (
+    <div>
+    <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <br/>
+        <label>Name:</label>
+        <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+        />
+        <br/>
+        <label >Username:</label>
+        <input
+            type="text"
+            id="username"
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+        />
+        <br/>
+        <label>Birthdate:</label>
+        <input
+            type="date"
+            id="birthdate"
+            value={birthdate}
+            onChange={(e) => setBirthdate(e.target.value)}
+        />
+        <br/>
+        <label >Password:</label>
+        <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+        />
+        <br/>
+        <label >Password Confirmation:</label>
+        <input
+            type="password"
+            id="password_confirmation"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+            autoComplete="current-password"
+        />
+        <br/>
+        <button type="submit">Sign Up</button>
+        </form>
+    </div>
+    );
+}
+
+export default SignUp;
